ea2361b139cded88c26d80af1bb7d6910215b9ce
Shelchin <hi@shelchin.com>
2025-10-21 12:20:49 +0800

    recator

--- src/lib.ts
+++ src/lib.ts
@@ -1,3 +1,12 @@
+import { S3Client, ListObjectsV2Command, DeleteObjectsCommand } from '@aws-sdk/client-s3';
+
+export interface Env {
+	ACCESS_KEY_ID: string;
+	SECRET_ACCESS_KEY: string;
+	ACCOUNT_ID: string;
+	BUCKET: string;
+	REGION: string;
+}
 interface HourPrefixOptions {
 	/** 获取多少小时的数据，默认 12 */
 	hours?: number;
@@ -38,3 +47,42 @@ export function getPastHourPrefixes(options: HourPrefixOptions = {}): string[] {
 
 	return prefixes;
 }
+
+export const deleteObjects = async (env: Env) => {
+	const S3 = new S3Client({
+		region: 'auto',
+		endpoint: `https://${env.ACCOUNT_ID}.${env.REGION}.r2.cloudflarestorage.com`,
+		credentials: {
+			accessKeyId: env.ACCESS_KEY_ID,
+			secretAccessKey: env.SECRET_ACCESS_KEY
+		}
+	});
+	const prefixs = getPastHourPrefixes({ hours: 6, excludeRecentHours: 2 });
+
+	console.log({ prefixs });
+	const keys: { Key: string }[] = [];
+	for (let i = 0; i < prefixs.length; i++) {
+		const list = await S3.send(
+			new ListObjectsV2Command({ Bucket: env.BUCKET, Prefix: prefixs[i] })
+		);
+		if (list?.Contents?.length ?? 0) {
+			for (let j = 0; j < list?.Contents!.length; j++) {
+				keys.push({
+					Key: list.Contents![j].Key as string
+				});
+			}
+		}
+	}
+
+	console.log('keys length=>', keys.length);
+
+	if (keys.length > 0) {
+		const deleteCommand = new DeleteObjectsCommand({
+			Bucket: env.BUCKET,
+			Delete: {
+				Objects: keys.slice(0, 1000)
+			}
+		});
+		await S3.send(deleteCommand);
+	}
+};
